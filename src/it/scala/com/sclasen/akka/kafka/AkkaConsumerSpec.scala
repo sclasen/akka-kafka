979ff0fc7eb5dd8b378b19cc7703401f3a58a9a6
Scott Clasen <scott@heroku.com>
2014-04-19 12:33:10 -0700

    add FlattenContinue state, add manual commit, cleanup logging, cleanup test

---
+++ src/it/scala/com/sclasen/akka/kafka/AkkaConsumerSpec.scala
@@ -0,0 +1,89 @@
+package com.sclasen.akka.kafka
+
+import org.scalatest._
+import kafka.producer.{ProducerConfig, Producer}
+import akka.actor.{Props, Actor, ActorRef, ActorSystem}
+import kafka.serializer.DefaultDecoder
+import kafka.producer.KeyedMessage
+import akka.testkit.{TestKit, ImplicitSender}
+import scala.concurrent.duration._
+import AkkaConsumerSpec._
+import com.sclasen.akka.kafka.StreamFSM.Processed
+
+
+class AkkaConsumerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll  {
+
+  def this() = this(ActorSystem("test"))
+
+  val topic = s"test${System.currentTimeMillis()}"
+
+  val producer = kafkaProducer
+
+  val messages = 1000
+
+  "AkkaConsumer" should {
+    "work" in {
+      val receiver = system.actorOf(Props(new TestReciever(testActor)))
+      val consumer = new AkkaConsumer(testProps(system, topic, receiver))
+      import system.dispatcher
+      consumer.start().map{
+        _ => testActor ! ConnectorFSM.Start
+      }
+      expectMsg(2 seconds, ConnectorFSM.Start)
+      sendMessages()
+      receiveN(messages, 5 seconds)
+    }
+  }
+
+  def sendMessages() {
+    (1 to messages).foreach{
+      num =>
+        producer.send(new KeyedMessage(topic, num.toString.getBytes, num.toString.getBytes))
+    }
+  }
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+}
+
+
+object AkkaConsumerSpec {
+
+  type Key = Array[Byte]
+  type Msg = Array[Byte]
+  type MsgProducer = Producer[Key,Msg]
+
+  def kafkaProducer = {
+    new MsgProducer(new ProducerConfig(kafkaProducerProps))
+  }
+
+  def kafkaProducerProps = AkkaConsumer.toProps(
+    "metadata.broker.list" -> "localhost:9092",
+    "producer.type" -> "sync",
+    "request.required.acks" -> "-1"
+  )
+
+  def testProps(system:ActorSystem, topic:String, receiver:ActorRef) = AkkaConsumerProps(
+    system,
+    "localhost:2181",
+    topic,
+    "consumer-spec",
+    2,
+    new DefaultDecoder(),
+    new DefaultDecoder(),
+    receiver, commitInterval = 1 second, commitAfterMsgCount = 66
+  )
+
+}
+
+class TestReciever(testActor:ActorRef) extends Actor{
+  override def receive = {
+    case m:Any =>
+      sender ! Processed
+      testActor ! m
+  }
+}
+
